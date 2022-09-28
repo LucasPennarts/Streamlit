22756ff5eadc7b6a1fe11b20e5af818098faca77
LucasPennarts <114577244+LucasPennarts@users.noreply.github.com>
2022-09-28 13:48:06 +0200

    Add files via upload

--- AppSetupV1.py
+++ AppSetupV1.py
@@ -5,7 +5,7 @@ Created on Wed Sep 28 12:22:07 2022
 @author: lpnnr
 """
 
-#import yfinance as yf
+import yfinance as yf
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -17,7 +17,30 @@ from plotly.subplots import make_subplots
 
 import plotly.io as pio
 
-#df_stock = yf.Ticker('TSLA').history(period = 'max', interval = '1d')
+
+url = 'https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue'
+html_data = requests.get(url).text
+
+beautiful_soup = BeautifulSoup(html_data,"html.parser")
+
+tables = beautiful_soup.find_all("table")
+
+for index,table in enumerate(tables):
+    if("Tesla Quarterly Revenue" in str(table)):
+        table_index=index
+tesla_revenue = pd.DataFrame(columns=["Date","Revenue"])
+
+for row in tables[table_index].tbody.find_all('tr'):
+    col=row.find_all("td")
+    if(col!=[]):
+        date=col[0].text
+        revenue=col[1].text.strip().replace("$","").replace(",","")
+        tesla_revenue=tesla_revenue.append({"Date":date,"Revenue":revenue}, ignore_index=True)
+tesla_revenue.head()
+
+tesla_revenue.dropna(inplace=True)
+not_empty=tesla_revenue["Revenue"]!=""
+tesla_revenue=tesla_revenue[not_empty]
 
 
 st.title("Streamlit Team 10")
@@ -51,11 +74,16 @@ st.write("Lorem Ipsum")
 
 options = st.multiselect("Kies dataset", ["Test1", "test2", "test3"])
 
-
+if options == "Test1":
+    st.write(tesla_revenue)
 
     
 
     
+chart_data = tesla_revenue
+
+st.line_chart(chart_data)
+
 
 
 
