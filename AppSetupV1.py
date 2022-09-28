02f471e2a4fad60b796594a9af91042f94ab2d97
LucasPennarts <114577244+LucasPennarts@users.noreply.github.com>
2022-09-28 13:35:53 +0200

    Add files via upload

--- AppSetupV1.py
+++ AppSetupV1.py
@@ -5,9 +5,20 @@ Created on Wed Sep 28 12:22:07 2022
 @author: lpnnr
 """
 
-import streamlit as st
+import yfinance as yf
 import pandas as pd
-import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
+
+from plotly.offline import plot
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import plotly.io as pio
+
+df_stock = yf.Ticker('TSLA').history(period = 'max', interval = '1d')
+
 
 st.title("Streamlit Team 10")
 st.header("Data keuze ")
@@ -19,32 +30,7 @@ st.write("Lorem Ipsum")
 
 "Imort dataframe"
 
-dftest = pd.read_csv("TestdataStreamlit.csv")
-
-
-# Store the initial value of widgets in session state
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.checkbox("Disable selectbox widget", key="disabled")
-    st.radio(
-        "Set selectbox label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
-
-with col2:
-    option = st.selectbox(
-        "How would you like to be contacted?",
-        ("Email", "Home phone", "Mobile phone"),
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-    )
-
+#dftest = pd.read_csv("TestdataStreamlit.csv")
 
 
 
@@ -66,12 +52,12 @@ with col2:
 options = st.multiselect("Kies dataset", ["Test1", "test2", "test3"])
 
 if options == "Test1":
-    st.write(dftest)
+    st.write(df_stock)
 
     
 
     
-chart_data = dftest
+chart_data = df_stock
 
 st.line_chart(chart_data)
 
